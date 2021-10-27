f345e1742b6079e6fc893b6b44b2f1f0001b97f1
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-10-27 15:19:52 +0000

    Card利用中止

--- admin/src/views/Login.vue
+++ admin/src/views/Login.vue
@@ -1,33 +1,27 @@
 <template>
   <div>
     <Dialog v-model:visible="display" :modal="true" :closable="false">
-      <Card class="p-shadow-7 p-p-4">
-        <template #header>ログインして下さい。</template>
-        <template #content>
-          <div class="p-mb-2">
-            <span class="p-float-label">
-              <InputText id="username" type="text" v-model="value" />
-              <label for="username">ユーザー名</label>
-            </span>
-          </div>
+      <template #header>ログインして下さい。</template>
 
-          <div class="p-mb-2">
-            <span class="p-float-label">
-              <InputText id="password" type="text" v-model="value" />
-              <label for="password">パスワード</label>
-            </span>
-          </div>
-        </template>
-        <template #footer>
+      <div class="p-mb-4">
+        <span class="p-float-label">
+          <InputText id="username" type="text" v-model="value" />
+          <label for="username">ユーザー名</label>
+        </span>
+      </div>
+
+      <div class="p-mb-2">
+        <span class="p-float-label">
+          <InputText id="password" type="text" v-model="value" />
+          <label for="password">パスワード</label>
+        </span>
+      </div>
+
+      <template #footer>
+        <div class="p-text-left">
           <Button icon="pi pi-check" label="ログイン" />
-          <Button
-            icon="pi pi-times"
-            label="キャンセル"
-            class="p-button-secondary"
-            style="margin-left: 0.5em"
-          />
-        </template>
-      </Card>
+        </div>
+      </template>
     </Dialog>
   </div>
 </template>
@@ -35,13 +29,12 @@
 <script>
 import { defineComponent } from "vue";
 import Dialog from "primevue/dialog";
-import Card from "primevue/card";
+
 import Button from "primevue/button";
 import InputText from "primevue/inputtext";
 export default defineComponent({
   components: {
     Button,
-    Card,
     Dialog,
     InputText,
   },
