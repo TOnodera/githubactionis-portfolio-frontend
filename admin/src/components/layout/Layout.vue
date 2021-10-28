3de8beaec01422cfe71b48088aa4fbe18de76176
Takeshi Onodera <t.onodera.1.2.1.5@gmail.com>
2021-10-28 02:26:05 +0000

    サイドバーにメニュー表示

--- admin/src/components/layout/Layout.vue
+++ admin/src/components/layout/Layout.vue
@@ -4,7 +4,9 @@
     <main>
       <slot name="main"></slot>
     </main>
-    <Sidebar v-model:visible="sidebarIsVisible" />
+    <Sidebar v-model:visible="sidebarIsVisible">
+      <MegaMenu :model="menu" orientation="vertical" />
+    </Sidebar>
     <Bottom />
   </div>
 </template>
@@ -14,15 +16,45 @@ import { defineComponent } from "vue";
 import Toolbar from "./Toolbar.vue";
 import Bottom from "./Bottom.vue";
 import Sidebar from "primevue/sidebar";
+import MegaMenu from "primevue/megamenu";
 export default defineComponent({
   components: {
     Toolbar,
     Bottom,
     Sidebar,
+    MegaMenu,
   },
   data() {
     return {
       sidebarIsVisible: false,
+      menu: [
+        {
+          label: "Videos",
+          icon: "pi pi-fw pi-video",
+          items: [
+            [
+              {
+                label: "Video 1",
+                items: [{ label: "Video 1.1" }, { label: "Video 1.2" }],
+              },
+              {
+                label: "Video 2",
+                items: [{ label: "Video 2.1" }, { label: "Video 2.2" }],
+              },
+            ],
+            [
+              {
+                label: "Video 3",
+                items: [{ label: "Video 3.1" }, { label: "Video 3.2" }],
+              },
+              {
+                label: "Video 4",
+                items: [{ label: "Video 4.1" }, { label: "Video 4.2" }],
+              },
+            ],
+          ],
+        },
+      ],
     };
   },
   methods: {
